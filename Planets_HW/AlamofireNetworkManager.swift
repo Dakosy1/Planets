01872b2f2b8a67b279926ab77a7e4d74e58895e2
Nurdaulet <ndauletkhan2004@mail.ru>
2024-03-27 15:34:38 +0500

    First commit

---
+++ Planets_HW/AlamofireNetworkManager.swift
@@ -0,0 +1,42 @@
+//
+//  AlamofireNetworkManager.swift
+//  Planets_HW
+//
+//  Created by Нурдаулет Даулетхан on 18.03.2024.
+//
+
+import Foundation
+import Alamofire
+
+class AlamofireNetworkManager {
+    
+    static let shared = AlamofireNetworkManager()
+
+    func getstarships(completion: @escaping (SWPlanetsResponse?) -> Void) {
+        let url = URL(string: "https://swapi.py4e.com/api/planets/")!
+
+        AF.request(url).response { response in
+
+            print(response.response?.statusCode)
+
+            if let data = response.data {
+                let decoder = JSONDecoder()
+                do {
+                    let planets = try decoder.decode(SWPlanetsResponse.self, from: data)
+                    completion(planets)
+                }catch {
+                    completion(nil)
+                }
+            }
+        }
+    }
+}
+struct SWPlanetsResponse: Codable {
+    var results: [Planets]
+}
+struct Planets: Codable{
+    var name: String
+    var diameter: String
+    var climate: String
+    var population: String
+}
